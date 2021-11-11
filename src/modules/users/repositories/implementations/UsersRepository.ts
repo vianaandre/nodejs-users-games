0d756d180c773d8ce19e51e15b996f4fcd88136c
vianaandre <andredapperviana@gmail.com>
2021-11-11 10:56:20 -0300

    feat: (challenge completed)

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,35 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return await this.repository.findOneOrFail({
+      relations: ['games'],
+      where: {
+        id: user_id,
+      }
+    });
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await await this.repository.query(`
+      SELECT 
+        *
+      FROM users
+      ORDER BY first_name ASC;
+    `);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`
+      SELECT 
+        * 
+      FROM users
+      WHERE 
+        first_name ILIKE $1
+        AND
+        last_name ILIKE $2
+    `, [first_name, last_name]);
   }
 }
